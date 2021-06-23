021a8c6316bc0f5ad8d412cfe8c880a4128c3b66
Chris McC <mcclelland.c@gmail.com>
2021-06-23 16:34:16 +0000

    Spike: rendering in isolation.

--- dist/esm/client/preview/render.js
+++ dist/esm/client/preview/render.js
@@ -1,53 +1,56 @@
 var _templateObject;
 
-function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+function _taggedTemplateLiteral(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
+  }
+  return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } }));
+}
 
-import "core-js/modules/es.object.assign.js";
-import "core-js/modules/es.function.name.js";
-import "core-js/modules/es.array.concat.js";
-import "core-js/modules/es.array.slice.js";
-import "core-js/modules/es.object.freeze.js";
+import App from 'app';
+App.reopen({ Router: false });
+
 import global from 'global';
 import dedent from 'ts-dedent';
-var globalWindow = global.window,
-    document = global.document;
-var rootEl = document.getElementById('root');
+var rootEl = global.document.getElementById('root');
 
-var config = globalWindow.require("".concat(globalWindow.STORYBOOK_NAME, "/config/environment"));
-
-var app = globalWindow.require("".concat(globalWindow.STORYBOOK_NAME, "/app")).default.create(Object.assign({
-  autoboot: false,
-  rootElement: rootEl
-}, config.APP));
-
-var lastPromise = app.boot();
+var app = App.create({
+  rootElement: '#lms',
+  modulePrefix: 'lms'
+});
 var hasRendered = false;
 var isRendering = false;
 
+let i = 0;
 function render(options, el) {
   if (isRendering) return;
   isRendering = true;
   var template = options.template,
-      _options$context = options.context,
-      context = _options$context === void 0 ? {} : _options$context,
-      element = options.element;
+    _options$context = options.context,
+    context = _options$context === void 0 ? {} : _options$context,
+    element = options.element;
 
   if (hasRendered) {
-    lastPromise = lastPromise.then(function (instance) {
-      return instance.destroy();
-    });
+    rootEl.innerText = '';
   }
 
-  lastPromise = lastPromise.then(function () {
-    var appInstancePrivate = app.buildInstance();
-    return appInstancePrivate.boot().then(function () {
-      return appInstancePrivate;
-    });
-  }).then(function (instance) {
-    instance.register('component:story-mode', Ember.Component.extend(Object.assign({
-      layout: template || options
-    }, context)));
-    var component = instance.lookup('component:story-mode');
+  console.log('options', options);
+
+  app.then(() => {
+    i++;
+    app.register(
+      `component:story-mode-${i}`,
+      Ember.Component.extend(
+        Object.assign(
+          {
+            layout: template || options
+          },
+          context
+        )
+      )
+    );
+
+    var component = app.__container__.lookup(`component:story-mode-${i}`);
 
     if (element) {
       component.appendTo(element);
@@ -58,22 +61,33 @@ function render(options, el) {
 
     hasRendered = true;
     isRendering = false;
-    return instance;
   });
 }
 
 export default function renderMain(_ref) {
   var storyFn = _ref.storyFn,
-      kind = _ref.kind,
-      name = _ref.name,
-      showMain = _ref.showMain,
-      showError = _ref.showError;
+    kind = _ref.kind,
+    name = _ref.name,
+    showMain = _ref.showMain,
+    showError = _ref.showError;
   var element = storyFn();
 
   if (!element) {
     showError({
-      title: "Expecting a Ember element from the story: \"".concat(name, "\" of \"").concat(kind, "\"."),
-      description: dedent(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n        Did you forget to return the Ember element from the story?\n        Use \"() => hbs('{{component}}')\" or \"() => { return {\n          template: hbs`{{component}}`\n        } }\" when defining the story.\n      "], ["\n        Did you forget to return the Ember element from the story?\n        Use \"() => hbs('{{component}}')\" or \"() => { return {\n          template: hbs\\`{{component}}\\`\n        } }\" when defining the story.\n      "])))
+      title: 'Expecting a Ember element from the story: "'
+        .concat(name, '" of "')
+        .concat(kind, '".'),
+      description: dedent(
+        _templateObject ||
+          (_templateObject = _taggedTemplateLiteral(
+            [
+              '\n        Did you forget to return the Ember element from the story?\n        Use "() => hbs(\'{{component}}\')" or "() => { return {\n          template: hbs`{{component}}`\n        } }" when defining the story.\n      '
+            ],
+            [
+              '\n        Did you forget to return the Ember element from the story?\n        Use "() => hbs(\'{{component}}\')" or "() => { return {\n          template: hbs\\`{{component}}\\`\n        } }" when defining the story.\n      '
+            ]
+          ))
+      )
     });
     return;
   }
